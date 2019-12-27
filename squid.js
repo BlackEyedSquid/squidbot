4eff2bf611683ae39bb0a3a4d4c7fd2f8f67f39e
BlackEyedSquid <blackeyedsquid@outlook.com>
2019-12-27 18:52:46 -0500

    Updated squid.js
    Added Reddit story to variable "Storys" at bottom of file

--- squid.js
+++ squid.js
@@ -5,7 +5,6 @@ const squid = new Discord.Client();
 const settings = require('./settings.json');
 // Will expand to include more swear words.
 const swearWords = ["fuck"]
-var randomStory = Storys[Math.floor(Math.random() * Storys.length)];
 
 squid.on("ready", () => {
     console.log("Ready to kick ass.");
@@ -34,6 +33,10 @@ squid.on("message", (message) => {
 
     if (message.content.startsWith(settings.prefix + "story")) {
         message.channel.send(randomStory);
+    } else
+
+    if (message.content.startsWith(swearWords)) {
+        message.channel.send("Umm, excuse me young one. Watch your mouth!");
     }
 });
 
@@ -424,6 +427,7 @@ squid.login(settings.token);
 
 
 
+
 // This bottom part is for extra variables because I am too stupid to understand how to get it in a different file.
 // If anyone has a solution to this problem, please DM on Discord at BlackEyedSquid#7096.
 
@@ -431,7 +435,8 @@ squid.login(settings.token);
 var Storys = [
     "ok so basically when I was a young little robot, me and my daddy played this game called \"Pee Pee Butt Touch\". I never understood what this game was, but me and daddy had a lot of fun. \n\nMy daddy told me to keep it a secret, because if I told people, we wouldn't be able to play it anymore. I vowed not to tell anyone. \n\nOne day, my mommy found out, she didn't want to join in, but she wanted to go to a place called \"Court\" and get something called a \"Divorce\" on daddy. \n\n\nI don't know what that means, but I hope it means I can get double VBucks for Christmas!",
     "oh god \ni gotta poop \n*proceeds to blast poop everywhere* \noh god it's on the walls \nhelp",
-    "oh shit oh fuck \n\noh shit oh fuck"
+    "oh shit oh fuck \n\noh shit oh fuck",
+    "one day, i was browse reddit and am disgusted because only coomers do reddit \n\n\n\nkeanu reeves big chungus wholesome 100 fortnite bad minecraft good ok boomer everyone disliked that."
 ];
 
 // Bot owner (BlackEyedSquid#7096) only commands
